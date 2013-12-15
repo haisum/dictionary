5edeca371babe3c5d1e8403b9e775d471d42fe47
haisum <haisumbhatti@gmail.com>
2013-12-15 12:55:43 +0400

    making changes according to the contents of ubuntu 13.10 installation on home laptop

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/haisum/worldslangs/venv"
+set -gx VIRTUAL_ENV "/home/haisum/dictionary/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
